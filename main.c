8b597e4f5ca682ca72d9de0136aff067aec03e7b
vesche <austinjackson892@gmail.com>
2018-09-30 19:15:58 -0400

    Non-authoritative movement working!

--- main.c
+++ main.c
@@ -14,13 +14,17 @@
 int main()
 {
     // const int FPS = 24;
-    // int frame_time = 0;
+    int frame_time = 0;
 
     // declare pointers
     SDL_Window *window;
     SDL_Renderer *renderer;
     SDL_Surface *surface;
     SDL_Texture *texture;
+
+    // sprite stuff
+    SDL_Rect player;
+    SDL_Rect player_position;
 
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
         print_error_msg(ERROR_TYPE_SDL2, "Couldn't initialize SDL");
@@ -52,8 +56,6 @@ int main()
     }
     
     // https://opengameart.org/content/tiny-16-basic
-    // surface = LoadTexture("sheet.png", renderer);
-
     surface = IMG_Load("sheet.png");
 
     if (surface == NULL) {
@@ -68,7 +70,7 @@ int main()
     SDL_FreeSurface(surface);
 
     if (texture == NULL) {
-        printf("error create texture: %s\n", SDL_GetError());
+        print_error_msg(ERROR_TYPE_SDL2, "Couldn't create texture");
         SDL_DestroyRenderer(renderer);
         SDL_DestroyWindow(window);
         SDL_Quit();
@@ -78,20 +80,23 @@ int main()
     int frame_width, frame_height;
     int texture_width, texture_height;
     SDL_QueryTexture(texture, NULL, NULL, &texture_width, &texture_height);
-    frame_width = texture_width / 32;
-    frame_height = texture_height / 32;
+    frame_width = texture_width / 16;   // 16 x 16 sprites on the sprite sheet
+    frame_height = texture_height / 16;
 
-    SDL_Rect player;
-    player.x = player.y = 0;
+    // player x, y, w, h
+    player.x = 0;   // location of player on sprite sheet (0, 256)
+    player.y = 256;
     player.w = frame_width;
     player.h = frame_height;
+
+    player_position.x = player_position.y = 0;  // current player position (0, 0)
+    player_position.w = player_position.h = 32; // size of player (32, 32)
 
     connect_to_server();
     
     // THREADING
     // https://github.com/raduprv/Eternal-Lands/blob/d277e1f8ff3cc257ac394e7393aa0d1442295b2a/main.c#L179
     // ------
-    // static void *network_thread_data = NULL;
     SDL_Thread *network_thread;
     network_thread = SDL_CreateThread(client_loop, "client_loop", (void *)NULL);
     if (network_thread == NULL) {
@@ -100,10 +105,8 @@ int main()
     }
     // ------
 
-    int x;
-    int y;
-    x = 0;
-    y = 0;
+    int player_speed;
+    player_speed = 4;
 
     bool quit = false;
 
@@ -126,35 +129,45 @@ int main()
                         quit = true;
                         break;
                     case SDLK_LEFT:
-                        x -= 1;
-                        // player.x -= 1;
+                        player.y = 288;
+                        player_position.x -= player_speed;
                         moved = true;
                         break;
                     case SDLK_RIGHT:
-                        x += 1;
-                        // player.x += 1;
+                        player.y = 320;
+                        player_position.x += player_speed;
                         moved = true;
                         break;
                     case SDLK_UP:
-                        y += 1;
+                        player.y = 352;
+                        player_position.y -= player_speed;
                         moved = true;
                         break;
                     case SDLK_DOWN:
-                        y -= 1;
+                        player.y = 256;
+                        player_position.y += player_speed;
                         moved = true;
                         break;
                 }
         }
 
         if (moved) {
-            char *message = payload_move(x, y);
+            char *message = payload_move(player_position.x, player_position.y);
             client_send(message);
+
+            frame_time++;
+            if (frame_time == 4) {
+                frame_time = 0;
+                player.x += frame_width;
+                if (player.x >= 96)
+                    player.x = 0;
+            }
         }
 
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
         // SDL_RenderCopy(renderer, texture, NULL, NULL);
-        SDL_RenderCopy(renderer, texture, &player, NULL);
+        SDL_RenderCopy(renderer, texture, &player, &player_position);
         SDL_RenderPresent(renderer);
     }
 
