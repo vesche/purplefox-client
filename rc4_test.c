dc8c8eb25064644d6a1f4f73e9ba7022241daf4a
vesche <vesche@protonmail.com>
2019-07-07 17:07:28 -0600

    fix og rc4 lib

--- rc4_test.c
+++ rc4_test.c
@@ -1,20 +1,64 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
-#include "rc4.c"
+// #include "rc4.c"
+
+#include "rc4_bak.c"
+
+char *strstrip(char *s)
+{
+        size_t size;
+        char *end;
+
+        size = strlen(s);
+
+        if (!size)
+                return s;
+
+        end = s + size - 1;
+        while (end >= s && isspace(*end))
+                end--;
+        *(end + 1) = '\0';
+
+        while (*s && isspace(*s))
+                s++;
+
+        return s;
+}
 
 int main()
 {
     char *plaintext = "This is my message";
-    char *key = "thisisatestkey";
+
+    char *key = "yellowsubmarine";
 
     unsigned char *ciphertext = malloc(sizeof(int) * strlen(plaintext));
 
-    RC4(key, plaintext, ciphertext);
+    for (size_t i = 0, len = strlen(plaintext); i < len; i++) {
+        printf("%02hhX", plaintext[i]);
+        //printf("%c", plaintext[i]);
+    }
+    printf("\n");
 
-    for (size_t i = 0, len = strlen(plaintext); i < len; i++)
+    // encrypt
+    RC4(key, plaintext, ciphertext);
+    for (size_t i = 0, len = strlen(plaintext); i < len; i++) {
         printf("%02hhX", ciphertext[i]);
+        //printf("%c", ciphertext[i]);
+    }
+    printf("\n");
+
+    // decrypt
+    unsigned char *test;
+    RC4(key, ciphertext, test);
+    test = strstrip(test);
+
+    for (size_t i = 0, len = strlen(test); i < len; i++) {
+        printf("%02hhX", test[i]);
+        //printf("%c", test[i]);
+    }
 
     return 0;
 }
